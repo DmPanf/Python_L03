a2c038198117d701583c9718bc57cd2c53fc375f
dnp34 <99917230+dnp34@users.noreply.github.com>
2023-01-26 20:14:11 +0300

    Update 1seq.py

--- 1seq.py
+++ 1seq.py
@@ -1,5 +1,6 @@
 Num = int(input('\nВведите количество элементов будущего списка: '))
 List = []
+print('Вводите любые цифры по одной.')
 for i in range(Num):
-    List.append(int(input(f'Введите {i+1} элемент: ')))
+    List.append(int(input(f'Введите {i + 1} элемент: ')))
 print(sorted(List))
