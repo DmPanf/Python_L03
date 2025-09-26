f87af34688c9c1973489527a2e6ab70fea409afa
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2025-09-27 01:18:42 +0300

    Delete 1seq.py

--- 1seq.py
+++
@@ -1,6 +0,0 @@
-Num = int(input('\nВведите количество элементов будущего списка: '))
-List = []
-print('Вводите любые цифры по одной.')
-for i in range(Num):
-    List.append(int(input(f'Введите {i + 1} элемент: ')))
-print(sorted(List))
